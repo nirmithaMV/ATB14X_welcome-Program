da7e492e9aec1e9667ca0db8fdbe37dae1f2b646
n.vinodh.kumar <nirmithamvkottur052@gmail.com>
2025-09-13 11:37:21 +0530

    Fizz buzz by lopp

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,18 +6,20 @@ public class FizzBuzz {
         System.out.println("Entered number is");
         int num = input.nextInt();
         System.out.println("Entered the input number"+ num);
-        if (num%3 ==0 && num%5==0) {
-            System.out.println("FizzBusZZ");
+        for(int i=1;i<=num;i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuZZ");
 
-        }
-        else if (num %3 ==0)
-        {
-            System.out.println("Fizz");
-        }
-        else if (num%5 ==0)
-        {
-            System.out.println("Buz");
-            
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buz");
+
+            }
+            else{
+                System.out.println(i);
+            }
         }
         input.close();
     }
