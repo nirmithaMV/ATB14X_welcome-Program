ceb1ef25db52a5897e8fc3941b4cbaf45d2a0338
n.vinodh.kumar <nirmithamvkottur052@gmail.com>
2025-09-20 10:55:29 +0530

    Class Notes 20th

---
+++ src/ATB14x_Tasks.java
@@ -0,0 +1,12 @@
+public class ATB14x_Tasks {
+    public static void main(String[] args) {
+        /*System.out.println((9*3/9+1)*3);
+        int b=12;
+        boolean c= !(b>10 || b<5);
+        System.out.println(c);*/
+        char a1='a';
+        char a2='b';
+        System.out.println(a1+a2);
+        return a1+a2;
+    }
+}
