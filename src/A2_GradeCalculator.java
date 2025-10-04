d500aeb3b0449e3fcc229926041ae7c6a395e697
n.vinodh.kumar <nirmithamvkottur052@gmail.com>
2025-10-04 19:59:30 +0530

    Gradecalculator

---
+++ src/A2_GradeCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class A2_GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println("Entered number is "+num);
+        if (num>100)
+        {
+            System.out.println("Invalid Number");
+        }
+        else
+        {
+            if (num>=90 && num<=100)
+            {
+                System.out.println("A");
+            } 
+            else if (num>=80 && num<=89)
+            {
+                System.out.println("B");
+                
+            } else if (num>=70 && num<=79) {
+                System.out.println("C");
+                
+            } else if (num>=60 && num<=69) {
+                System.out.println("D");
+                
+            }
+            else
+            {
+                System.out.println("Fail");
+            }
+
+        }
+    }
+}
